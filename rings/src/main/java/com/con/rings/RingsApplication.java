e9ed71b2f75c45c75af1b015d189ab3fde375a88
McDog3 <McDog3@users.noreply.github.com>
2018-11-24 08:56:59 -0600

    Initial commit via upload

---
+++ rings/src/main/java/com/con/rings/RingsApplication.java
@@ -0,0 +1,12 @@
+package com.con.rings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingsApplication.class, args);
+	}
+}
